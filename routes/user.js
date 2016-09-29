63458bc60e0086bc612f516ebd6ad64d10faf1bb
lucas geitner <lucas.geitner@outlook.com>
2016-09-29 16:47:49 +0200

    let's try easy

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/mydb");
-
-follorwerSchema = {
-  id_twitter: { type: Number, validate : function(value, respond){
-    var id_twitter = this.id_twitter;
-    Follower.findOne({id_twitter: id_twitter}, function(err, follower){
-      if (follower) respond(false);
-      else respond(true);
-    });
-  }},
-  done: Boolean,
-};
-
-var Follower = mongoose.model("Follower", follorwerSchema);
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb://localhost/mydb");
+//
+// follorwerSchema = {
+//   id_twitter: { type: Number, validate : function(value, respond){
+//     var id_twitter = this.id_twitter;
+//     Follower.findOne({id_twitter: id_twitter}, function(err, follower){
+//       if (follower) respond(false);
+//       else respond(true);
+//     });
+//   }},
+//   done: Boolean,
+// };
+//
+// var Follower = mongoose.model("Follower", follorwerSchema);
 
 // URL
 
-
-exports.list = function(req, res){
-  res.send("Lalala");
-  var c = new Follower({id_twitter: 200000, done: true});
-  console.log(c);
-  c.save(function(err){
-    console.log("2");
-    if(err){
-      for (var prop in err.errors){
-        var message = err.errors[prop].message;
-        console.log(message);
-      }
-    }
-    console.log("Liste des fans : ");
-    Follower.find(function(err, fans){
-      console.log(fans);
-    })
-  })
-};
+//
+// exports.list = function(req, res){
+//   res.send("Lalala");
+//   var c = new Follower({id_twitter: 200000, done: true});
+//   console.log(c);
+//   c.save(function(err){
+//     console.log("2");
+//     if(err){
+//       for (var prop in err.errors){
+//         var message = err.errors[prop].message;
+//         console.log(message);
+//       }
+//     }
+//     console.log("Liste des fans : ");
+//     Follower.find(function(err, fans){
+//       console.log(fans);
+//     })
+//   })
+// };
